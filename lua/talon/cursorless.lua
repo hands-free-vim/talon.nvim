a59bbbac885bd7e8268acd79e3d33a3e29cac7b9
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-03-15 17:35:26 +0000

    feat: load command server and cursorless on setup

--- lua/talon/cursorless.lua
+++ lua/talon/cursorless.lua
@@ -1,7 +1,11 @@
 local M = {}
 
 function M.setup()
-    print("Setting up cursorless")
+    -- these prints are useful when debugging since we need to attach to let neovim run at startup
+    print("Setting up cursorless...")
+    vim.api.nvim_call_function("CursorlessLoadExtension", {})
+    vim.api.nvim_call_function("CommandServerLoadExtension", {})
+    print("Setting up cursorless done")
 end
 
 return M
