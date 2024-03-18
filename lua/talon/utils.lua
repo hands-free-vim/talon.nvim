1525005507393b76c7f98c7b0df0f920e74511b2
Aaron Adams <aaron.adams@nccgroup.com>
2024-03-18 13:24:19 +0800

    chore: linting

--- lua/talon/utils.lua
+++ lua/talon/utils.lua
@@ -1,29 +1,35 @@
 local M = {}
 
 function M.dump_table(o, depth, max_depth)
-	local seen_tables = {}
-	local function dump(o, depth, max_depth)
-		depth = depth or 1
-		if max_depth and depth > max_depth then
-			return "{ ... }"
-		end
-		if seen_tables[o] then
-			return "{ DUPLICATE }"
-		end
-		seen_tables[o] = true
-		if type(o) == "table" then
-			local s = "{ \n"
-			for k, v in pairs(o) do
-				if type(k) ~= "number" then
-					k = '"' .. k .. '"'
-				end
-				s = s .. string.rep(" ", depth * 2) .. "[" .. k .. "] = " .. dump(v, depth + 1, max_depth) .. ",\n"
-			end
-			return s .. string.rep(" ", (depth - 1) * 2) .. "}"
-		else
-			return tostring(o)
-		end
-	end
-	return dump(o, depth, max_depth)
+    local seen_tables = {}
+    local function dump(o, depth, max_depth)
+        depth = depth or 1
+        if max_depth and depth > max_depth then
+            return "{ ... }"
+        end
+        if seen_tables[o] then
+            return "{ DUPLICATE }"
+        end
+        seen_tables[o] = true
+        if type(o) == "table" then
+            local s = "{ \n"
+            for k, v in pairs(o) do
+                if type(k) ~= "number" then
+                    k = '"' .. k .. '"'
+                end
+                s = s
+                    .. string.rep(" ", depth * 2)
+                    .. "["
+                    .. k
+                    .. "] = "
+                    .. dump(v, depth + 1, max_depth)
+                    .. ",\n"
+            end
+            return s .. string.rep(" ", (depth - 1) * 2) .. "}"
+        else
+            return tostring(o)
+        end
+    end
+    return dump(o, depth, max_depth)
 end
 return M
